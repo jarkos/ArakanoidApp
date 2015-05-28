a5172683f768b08bf840fc7c0bd23b37da28759f
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-05-28 20:24:51 +0200

    Added menu and GameActivity

--- app/src/main/java/com/example/jarek/arakanoidapp/GameActivity.java
+++ app/src/main/java/com/example/jarek/arakanoidapp/GameActivity.java
@@ -1,37 +1,24 @@
 package com.example.jarek.arakanoidapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-    Button btn;
+public class GameActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                Intent myIntent = new Intent(MainActivity.this, GameActivity.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
+        setContentView(R.layout.activity_game);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_game, menu);
         return true;
     }
 
